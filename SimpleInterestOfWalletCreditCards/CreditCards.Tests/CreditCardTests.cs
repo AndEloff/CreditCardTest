abeb6ec7d921820b02b0cd18a2d3949f3fa6a99a
Eloff <andeloff@gmail.com>
2015-12-29 11:15:47 -0600

    Simple Credit Card Interest Coding Challenge
    Uploading Soultion

---
+++ SimpleInterestOfWalletCreditCards/CreditCards.Tests/CreditCardTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Core;
+using CreditCardsWithinWallet;
+
+namespace CreditCards.Tests
+{
+    [TestFixture]
+    public class CreditCardTests
+    {
+        [Test]
+        public void One_Person_One_Wallet_Three_Cards_Simple_Intrest()
+        {
+            var personsWallet = new CreditCard();
+            var walletInterestResult = Convert.ToDouble(personsWallet.GetWalletIntrestOfThreeCards(personsWallet.Mastercard(), personsWallet.DiscoveryCard(), personsWallet.VisaCard()));
+            Assert.That(walletInterestResult, Is.EqualTo(16.00));
+        }
+
+        [Test]
+        public void One_Person_Two_Wallets_Simple_Intrest_Of_Each_Wallet()
+        {
+            var firstWallet = new CreditCard();
+            var secondWallet = new CreditCard();
+            var firstWalletResult = Convert.ToDouble(firstWallet.GetWalletIntrestOfTwoCards(firstWallet.Mastercard(), firstWallet.DiscoveryCard()));
+            Assert.That(firstWalletResult, Is.EqualTo(6.00));
+            var secondWalletResult = Convert.ToDouble(secondWallet.GetWalletInterestOfOneCard(secondWallet.VisaCard()));
+            Assert.That(secondWalletResult, Is.EqualTo(10.00));
+        }
+
+        [Test]
+        public void Two_People_With_Their_Own_Wallet_Simple_Intrest()
+        {
+            var firstPerson = new CreditCard();
+            var secondPerson = new CreditCard();
+            var firstPersonResult = Convert.ToDouble(firstPerson.GetWalletIntrestOfThreeCards(firstPerson.Mastercard(), firstPerson.Mastercard(), firstPerson.VisaCard()));
+            Assert.That(firstPersonResult, Is.EqualTo(20.00));
+            var secondPersonResult = Convert.ToDouble(secondPerson.GetWalletIntrestOfTwoCards(secondPerson.Mastercard(), secondPerson.VisaCard()));
+            Assert.That(secondPersonResult, Is.EqualTo(15.00));
+        }
+    }
+}
---
+++ SimpleInterestOfWalletCreditCards/UserWallet/CreditCard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CreditCardsWithinWallet
+{
+    public class CreditCard
+    {
+        public double creditCardInterest { get; set; }
+        public double creditCardBalance { get; set; }
+
+        public double Mastercard()
+        {
+            creditCardInterest = .05;
+            creditCardBalance = 100;
+            double masterCardSimpleInterest = creditCardBalance * creditCardInterest;
+            return masterCardSimpleInterest;
+        }
+
+        public double DiscoveryCard()
+        {
+            creditCardInterest = .01;
+            creditCardBalance = 100;
+            double discoveryCardSimpleInterest = creditCardBalance * creditCardInterest;
+            return discoveryCardSimpleInterest;
+        }
+
+        public double VisaCard()
+        {
+            creditCardInterest = .10;
+            creditCardBalance = 100;
+            double visaSimpleInterest = creditCardBalance * creditCardInterest;
+            return visaSimpleInterest;
+        }
+
+        public int GetWalletIntrestOfTwoCards(double cardA, double cardB)
+        {
+            double creditCardA = cardA;
+            double creditCardB = cardB;
+            double creditCardInterest = creditCardA + creditCardB;
+            return Convert.ToInt32(creditCardInterest);
+        }
+
+        public int GetWalletIntrestOfThreeCards(double cardA, double cardB, double cardC)
+        {
+            double creditCardA = cardA;
+            double creditCardB = cardB;
+            double creditCardC = cardC;
+            double creditCardInterest = creditCardA + creditCardB + creditCardC;
+            return Convert.ToInt32(creditCardInterest);
+        }
+
+        public int GetWalletInterestOfOneCard(double a)
+        {
+            double creditCardA = a;
+            return Convert.ToInt32(creditCardA);
+        }
+    }
+}
